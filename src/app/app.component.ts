f7a2bd937013c0c2decf2ae8d1fe6af2c0ca2547
Justin Smith <justinpetersmith@gmail.com>
2024-12-01 15:53:17 -0500

    fixed typo in welcome page and updated blurb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnChanges } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
